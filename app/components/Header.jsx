b4e0de72468104a9d8d432e3e4615c9bf310cb59
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2025-01-29 23:13:43 +0530

    Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
               viewBox="0 0 24 24"
               strokeWidth="2"
               stroke="currentColor"
-              className={`w-6 h-6 ${isMenuOpen ? "mb-4" : ""}`}
+              className={`w-6 h-6 ${isMenuOpen ? "mb-36" : ""}`}
             >
               {isMenuOpen ? (
                 <path
